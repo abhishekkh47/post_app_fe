216e7d850eae9ea1ad1bba2546c8f1a59aa8a452
Abhishek Khandelwal <abhishek.khandelwal@mindinventory.com>
2025-03-24 17:10:09 +0530

    save image as base 64 in local storage

--- src/types/common.ts
+++ src/types/common.ts
@@ -2,3 +2,9 @@ export interface GroupDetails {
   name: string;
   description: string;
 }
+
+export interface ImagePreviewData {
+  id: string;
+  imagePreviews: string[];
+  isBase64: boolean;
+}
---
+++ src/utils/helpers.ts
@@ -0,0 +1,9 @@
+// Convert File to Base64
+export const fileToBase64 = (file: File): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = (error) => reject(error);
+  });
+};
