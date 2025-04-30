ba3c7af3168a5802211d6b69bd94c45fd6c36332
Abhishek Khandelwal <abhishek.khandelwal@mindinventory.com>
2025-04-30 17:49:22 +0530

    update scrollbar for home and profile page

--- src/components/layout/AuthenticatedLayout.tsx
+++ src/components/layout/AuthenticatedLayout.tsx
@@ -16,16 +16,20 @@ const AuthenticatedLayout: React.FC<AuthenticatedLayoutProps> = ({
   return (
     <>
       <NavBar />
-      <div className="hidden lg:block fixed top-16 left-0 w-48 xl:w-72 2xl:w-96 h-[calc(100vh-4rem)] bg-white transition-all duration-300 ease-in-out overflow-y-auto z-10">
-        <LeftPanel
-          showCreatePostModal={showCreatePostModal}
-          handleCancelPost={handleCancelPost}
-          openCreatePostModal={openCreatePostModal}
-        />
+      <div className="hidden lg:block fixed top-16 left-0 w-48 xl:w-72 2xl:w-96 h-[calc(100vh-4rem)] bg-white transition-all duration-300 ease-in-out z-10">
+        <div className="h-full overflow-y-auto custom-scrollbar">
+          <LeftPanel
+            showCreatePostModal={showCreatePostModal}
+            handleCancelPost={handleCancelPost}
+            openCreatePostModal={openCreatePostModal}
+          />
+        </div>
       </div>
       <div className="min-h-screen bg-gray-100 sm:pt-16 pt-10">{children}</div>
-      <div className="hidden lg:block fixed top-16 right-0 w-48 xl:w-72 2xl:w-96 h-[calc(100vh-4rem)] bg-white transition-all duration-300 ease-in-out overflow-y-auto z-10">
-        <RightPanel />
+      <div className="hidden lg:block fixed top-16 right-0 w-48 xl:w-72 2xl:w-96 h-[calc(100vh-4rem)] bg-white transition-all duration-300 ease-in-out z-10">
+        <div className="h-full overflow-y-auto custom-scrollbar">
+          <RightPanel />
+        </div>
       </div>
     </>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,9 +14,11 @@ const Home: React.FC = () => {
   const { joinGroupData } = useJoinGroup();
 
   return (
-    <div className="w-full lg:pl-48 xl:pl-72 2xl:pl-96 lg:pr-48 xl:pr-72 2xl:pr-96 transition-all duration-300">
-      <div className="max-w-3xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <PostFeed />
+    <div className="w-full h-[calc(100vh-5rem)] lg:pl-48 xl:pl-72 2xl:pl-96 lg:pr-48 xl:pr-72 2xl:pr-96 transition-all duration-300">
+      <div className="h-full overflow-y-auto custom-scrollbar">
+        <div className="max-w-3xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <PostFeed />
+        </div>
       </div>
       <ChatDrawer user={user} />
       {showGroupJoinModal && inviteToken && (
