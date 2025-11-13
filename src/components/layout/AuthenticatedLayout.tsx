ddeb6cb8018d60cf51d7681c88dfd3c67ef1fbf5
abhishekkhmind <abhishek.khandelwal@mindinventory.com>
2025-11-13 17:32:45 +0530

    fix scroll issues

--- src/components/layout/AuthenticatedLayout.tsx
+++ src/components/layout/AuthenticatedLayout.tsx
@@ -25,7 +25,8 @@ const AuthenticatedLayout: React.FC<AuthenticatedLayoutProps> = ({
           />
         </div>
       </div>
-      <div className="min-h-screen bg-gray-100 sm:pt-16 pt-10">{children}</div>
+      <div className="min-h-screen bg-gray-100 sm:pt-10 pt-10">{children}</div>
+      {/* <div className="h-screen bg-gray-100 sm:pt-10 pt-10 overflow-hidden">{children}</div> */}
       <div className="hidden lg:block fixed top-16 right-0 w-48 xl:w-72 2xl:w-96 h-[calc(100vh-4rem)] bg-white transition-all duration-300 ease-in-out z-10">
         <div className="h-full overflow-y-auto custom-scrollbar">
           <RightPanel />
--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -6,34 +6,38 @@ import { Loader } from "../components/common";
 const Explore: React.FC = () => {
   const { loading, friendSuggestions, handleFriendClick } = useFriends();
   return (
-    <div className="max-w-2xl mx-auto py-8 px-4">
-      {loading ? (
-        <Loader />
-      ) : (
-        <>
-          {/* Toggle Buttons */}
-          {/* <div className="flex space-x-2">
-            <button
-              className={`px-4 py-2 rounded-md font-semibold transition bg-indigo-600 text-white`}
-            >
-              Accounts
-            </button>
-          </div> */}
-
-          {/* <FriendSuggestions /> */}
-          {friendSuggestions?.length > 0 && (
+    <div className="w-full h-[calc(100vh-5rem)] lg:pl-48 xl:pl-72 2xl:pl-96 lg:pr-48 xl:pr-72 2xl:pr-96 transition-all duration-300">
+      <div className="h-full overflow-y-auto custom-scrollbar">
+        <div className="max-w-2xl mx-auto py-8 px-4">
+          {loading ? (
+            <Loader />
+          ) : (
             <>
-              <h1 className="text-xl text-gray-900 text-start">
-                People you may know
-              </h1>
-              <FriendSuggestions
-                friendSuggestions={friendSuggestions}
-                handleFriendClick={handleFriendClick}
-              />
+              {/* Toggle Buttons */}
+              {/* <div className="flex space-x-2">
+                <button
+                  className={`px-4 py-2 rounded-md font-semibold transition bg-indigo-600 text-white`}
+                >
+                  Accounts
+                </button>
+              </div> */}
+
+              {/* <FriendSuggestions /> */}
+              {friendSuggestions?.length > 0 && (
+                <>
+                  <h1 className="text-xl text-gray-900 text-start">
+                    People you may know
+                  </h1>
+                  <FriendSuggestions
+                    friendSuggestions={friendSuggestions}
+                    handleFriendClick={handleFriendClick}
+                  />
+                </>
+              )}
             </>
           )}
-        </>
-      )}
+        </div>
+      </div>
     </div>
   );
 };
